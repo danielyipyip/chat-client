3fe762b63d3c92715b8f531ab3a0332c27cc1c69
Daniel YL YIP <2602505y@student.gla.ac.uk>
2022-04-20 17:01:00 +0100

    basic bidirectional message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ChatArea from "./components/ChatArea";
 
 import io from 'socket.io-client';
 import {useSelector, useDispatch} from 'react-redux'
-import {LoadMsg} from './components/redux/index'
+import {LoadMsg, setRoom, setUser} from './components/redux/index'
 
 function App() {
   let socket;
@@ -17,29 +17,59 @@ function App() {
   const socketRef = useRef();
   const [messages, setMessages] = useState([]);
   const [text, setText] = useState("");
+  // useEffect(()=>{
+  //   socket.on('messageFromServer', msg=>{
+  //     console.log(`received ${msg}`)
+  //     setMessages(prev => [...prev, msg])
+  //   })
+  // },[socket])
+
   useEffect(()=>{
-    socket.on('messageFromServer', msg=>{
-      console.log(`received ${msg}`)
+    socket.on('chatMessage', msg=>{
       setMessages(prev => [...prev, msg])
     })
-  },[socket])
+  }, [socket])
 
   const handleSubmit = (evt) =>{
     evt.preventDefault();
     // console.log(evt.target[0].value);
     if (text){
-      socketRef.current.emit('message', evt.target[0].value)
+      socketRef.current.emit('chatMessage', evt.target[0].value, room)
     }
     
+  }
+
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.demographic.user)
+  const handleSetUser = (evt) =>{
+    evt.preventDefault();
+    dispatch(setUser(evt.target[0].value));
+  }
+  const room = useSelector(state => state.demographic.room)
+  const handleSetRoom = (evt) =>{
+    evt.preventDefault();
+    dispatch(setRoom(evt.target[0].value));
+    console.log(evt.target[0].value)
+    socketRef.current.emit('joinroom', user, evt.target[0].value)
   }
   return (
     <div className="App">
         App
         <ChatArea/>
+        <p>User: {user}</p>
+        <form onSubmit={evt=>handleSetUser(evt)}>
+            <input type='text'></input>
+            <button type='submit'>setUser</button>
+        </form>
+        <form onSubmit={evt=>handleSetRoom(evt)}>
+            <input type='text'></input>
+            <button type='submit'>setChatRoom</button>
+        </form>
         <form onSubmit={evt=>handleSubmit(evt)}>
             <input type='text' value={text} onChange={evt=>setText(evt.target.value)}></input>
             <button type='submit'>Submit</button>
         </form>
+        <p>room: {room}</p>
         <div>
           {messages.map(msg=>{
             return <p>{msg}</p>
